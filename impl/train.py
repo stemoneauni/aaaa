52259934474ee00be308aaf5e7ee4694e54dbc72
Stefano Francesco Monea <mnosfn01r31c710q@studenti.unical.it>
2025-11-10 18:12:35 +0000

    base

---
+++ impl/train.py
@@ -0,0 +1,34 @@
+import torch
+
+
+def train(optimizer, model, dataloader, loss_fn):
+    '''
+    Train models in an epoch.
+    '''
+    model.train()
+    total_loss = []
+    for batch in dataloader:
+        optimizer.zero_grad()
+        pred = model(*batch[:-1], id=0)
+        loss = loss_fn(pred, batch[-1])
+        loss.backward()
+        total_loss.append(loss.detach().item())
+        optimizer.step()
+    return sum(total_loss) / len(total_loss)
+
+
+@torch.no_grad()
+def test(model, dataloader, metrics, loss_fn):
+    '''
+    Test models either on validation dataset or test dataset.
+    '''
+    model.eval()
+    preds = []
+    ys = []
+    for batch in dataloader:
+        pred = model(*batch[:-1])
+        preds.append(pred)
+        ys.append(batch[-1])
+    pred = torch.cat(preds, dim=0)
+    y = torch.cat(ys, dim=0)
+    return metrics(pred.cpu().numpy(), y.cpu().numpy()), loss_fn(pred, y)
